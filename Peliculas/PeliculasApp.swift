d18cb4c8e2dbf9d46037857cc3982909ae1a064c
Mariana Valencia Echeverri <mvalencia@lulobank.com>
2024-07-22 17:18:44 -0500

    Implementación iniciar del patrón de arquitectura MVVM

---
+++ Peliculas/PeliculasApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeliculasApp.swift
+//  Peliculas
+//
+//  Created by Mariana Valencia Echeverri on 22/07/24.
+//
+
+import SwiftUI
+
+@main
+struct PeliculasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            Home()
+        }
+    }
+}
